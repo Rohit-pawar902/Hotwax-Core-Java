b5fe590442e87286505a858750ec9d81a68f45ab
Rohit pawar <rohitpawar28112000@gmail.com>
2022-03-08 15:54:46 +0530

    created Employee.java and EmployeeManagement.java now working on defination's

--- Assigment3/EmployeeManagement.java
+++ Assigment3/EmployeeManagement.java
@@ -1,5 +1,21 @@
+import java.io.*;
+import java.util.*;
+
 class EmployeeManagement{
-   
+    private HashMap<Long,Employee> EmpList;
+    
+    EmployeeManagement(){
+        
+    }
+    
+    public void showMenu(){
+        System.out.println("Choose/Enter Operation id to perform.....");
+        System.out.println("1).Add an Employee");
+        System.out.println("2).Delete an Employee");
+        System.out.println("3).Search an Employee");
+        System.out.println("4).Update an Employee by its ID");
+        System.out.println("5).exit");
+    }
 
     public static void main(String[] args) {
         
